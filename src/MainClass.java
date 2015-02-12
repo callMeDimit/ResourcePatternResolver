58564ae5c3186dced687bda64bb554330f5fa997
gzeyu <gzeyu@gzeyu-PC>
2015-02-12 19:12:03 +0800

    搭建资源搜索器环境

---
+++ src/MainClass.java
@@ -0,0 +1,14 @@
+
+
+import com.dimit.PathMatchingResourcePatternResolver;
+import com.dimit.ResourcePatternResolver;
+
+/**
+ * Dimit 2015年2月12日
+ */
+public class MainClass {
+	public static void main(String[] args) {
+		 ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+	}
+}
+
